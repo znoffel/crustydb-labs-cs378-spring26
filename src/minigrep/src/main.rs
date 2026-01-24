e55f1310b95338ff5e217273cb7450c82aaaa6db
Zakarya Noffel <znoffel@gmail.com>
2026-01-24 15:14:19 -0600

    lab 0 minigrep

--- src/minigrep/src/main.rs
+++ src/minigrep/src/main.rs
@@ -1,5 +1,71 @@
+use std::env;
+use std::fs;
+use std::process;
+use std::error::Error;
+use minigrep::{search, search_case_insensitive};
+
+
 fn main() {
-    panic!("TODO: add your code to implement minigrep")
+    // let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+}
